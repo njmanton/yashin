ef600ed18d636fb44ab4eea66db7353c23ab7457
Nick Manton <nick@mantonbradbury.com>
2018-04-23 15:17:00 +0100

    add flags and opponents to goals list

--- controllers/GoalsController.js
+++ controllers/GoalsController.js
@@ -9,14 +9,28 @@ const controller = {
   get_index: function(req, res) {
     models.Goal.findAll({
       attributes: ['id', 'scorer', 'time', 'tao', 'type'],
+      raw: true,
       include: [{
         model: models.Team,
         attributes: ['id', 'name', 'sname'],
       }, {
         model: models.Match,
-        attributes: ['id', 'date']
+        attributes: ['id', 'date'],
+        include: [{
+          model: models.Team,
+          as: 'TeamA',
+          attributes: ['id', 'name', 'sname']
+        }, {
+          model: models.Team,
+          as: 'TeamB',
+          attributes: ['id', 'name', 'sname']
+        }]
       }]
     }).then(goals => {
+      goals.map(goal => {
+        goal.oppo = goal['team.name'] == goal['match.TeamA.name'] ? goal['match.TeamB.name'] : goal['match.TeamA.name'];
+        goal.oppoflag = goal['team.name'] == goal['match.TeamA.name'] ? goal['match.TeamB.sname'] : goal['match.TeamA.sname'];
+      })
       res.render(folder + '/index', {
         data: goals,
         title: 'All Goals',
