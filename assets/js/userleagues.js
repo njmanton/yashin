7f0ed0ffcebafed439f13661ac24327ae9adeb49
Nick Manton <nick@mantonbradbury.com>
2018-05-20 11:55:13 +0100

    add status icon to user leagues in /home

--- assets/js/userleagues.js
+++ assets/js/userleagues.js
@@ -1,8 +1,8 @@
 /* eslint-env browser, jquery */
-/* eslint prefer-template: 0 */
-
+/* eslint prefer-template: 0 no-console: 0 */
 'use strict';
 
+// runs on GET /home to populate user leagues
 $(document).ready(function() {
 
   var uid  = $('h3').data('uid'),
@@ -11,7 +11,8 @@ $(document).ready(function() {
   $.get(`/users/${ uid }/leagues`).done(function(leagues) {
     if (leagues.length) {
       $.each(leagues, function(k, v) {
-        list.append(`<li><a href="leagues/${ v.league.id }">${ v.league.name }</a></li>`);
+        const status = v.league.public ? '' : '<i class="fa fa-lock"></i>';
+        list.append(`<li><a href="leagues/${ v.league.id }">${ v.league.name }</a> ${ status }</li>`);
       });
     } else {
       list.replaceWith('<p>You are not a member of any user leagues yet</p>');
